c952dc04aa605449253d5eaf3fabe38b761b2f51
tanisshaaa <95009996+tanisshaaa@users.noreply.github.com>
2022-08-24 12:59:00 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Account
+{
+public:
+float salary=60000;
+};
+class Programmer:public Account
+{
+public:
+float bonus=5000;
+};
+int main(void)
+{
+Programmer p1;
+cout<<"salary: "<<p1.salary<<endl;
+cout<<"bonus: "<<p1.bonus<<endl;
+return 0;
+}
---
+++ code_using_namespace.cpp
@@ -0,0 +1,22 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter two numbers:";
+cin>>a>>b;
+cout<<"enter numbers are"<<a<<"\n"
+<<b;
+return 0;
+}*/
+
+#include<iostream>
+int main()
+{
+int a,b;
+std::cout<<"enter two numbers:";
+std::cin>>a>>b;
+std::cout<<"enter numbers are"<<a<<"\n"
+<<b;
+return 0;
+}
